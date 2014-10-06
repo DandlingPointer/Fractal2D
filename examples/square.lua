21f0e4b695e5f48724f8df13cf6d3e18d4695b9f
Lennart Vogelsang <lennart@vogelsangs.net>
2014-10-06 08:17:20 +0200

    stuff

---
+++ examples/square.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: vogelsang
+-- Date: 15.09.2014
+-- Time: 09:35
+-- To change this template use File | Settings | File Templates.
+--
+
+if x >= 0.0 and x <= 0.5 and y >= 0.25 and y <= 0.5 then
+    r = 0.5
+    g = 0.5
+    b = 0.5
+end
+r = 0.0
+g = 0.0
+b = 0.0
+
